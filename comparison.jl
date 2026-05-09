dcb7ab6767412465e32254b2769a6dfcbb880ffa
Camilla Berti <camillaberti@TABLET-ULB3CO72.localdomain>
2026-05-09 17:46:31 +0200

    defined reduced operators

--- comparison.jl
+++ comparison.jl
@@ -34,4 +34,63 @@ idx2super = Dict(i => s for (i,s) in enumerate(super_basis))
 println(super_basis)
 println("Full super space: $(length(ket_basis)^2) states")
 println("Restricted super space: $(length(super_basis)) states")
-#step 3 is to build operators in the restricted super space
+"""
+step 3 is to build operator in the resticted super space. I did two functions, one for left operators and one for right. Essentially they take
+two dictionaries (state => idx and idx => state) and the site (to create a1, a2 ... for each site)
+we create the matrix a_site
+"""
+
+function s_destroy_right(super2idx, idx2super, site)
+    D = length(super2idx) # dimension of reduced hilbert space
+    I_list, J_list, V_list = Int[], Int[], ComplexF64[]
+    
+
+    for (j, (ket, bra)) in idx2super
+        n_k = bra[site]
+        if n_k > 0                           # same check as enr_destroy: s > 0
+            new_bra = setindex(bra, n_k-1, site) #returns a new vector (new_ket) with value nk-1 at index site
+            new_key = (ket, new_bra)
+            if haskey(super2idx, new_key)    # check output is in restricted basis
+                i = super2idx[new_key]
+                push!(I_list, i)
+                push!(J_list, j)
+                push!(V_list, sqrt(ComplexF64(n_k)))
+            end
+        end
+    end
+
+    return QuantumObject(sparse(I_list, J_list, V_list, D, D), Operator())
+end
+
+
+function s_destroy_left(super2idx, idx2super, site)
+    D = length(super2idx) # dimension of reduced hilbert space
+    I_list, J_list, V_list = Int[], Int[], ComplexF64[]
+    
+
+    for (j, (ket, bra)) in idx2super
+        n_k = ket[site]
+        if n_k > 0                           # same check as enr_destroy: s > 0
+            new_ket = setindex(ket, n_k-1, site) #returns a new vector (new_ket) with value nk-1 at index site
+            new_key = (new_ket, bra)
+            if haskey(super2idx, new_key)    # check output is in restricted basis
+                i = super2idx[new_key]
+                push!(I_list, i)
+                push!(J_list, j)
+                push!(V_list, sqrt(ComplexF64(n_k)))
+            end
+        end
+    end
+
+    return QuantumObject(sparse(I_list, J_list, V_list, D, D), Operator())
+end
+#operators I defined in the reduced space
+a1_left = s_destroy_left(super2idx, idx2super, 1)
+a2_left = s_destroy_left(super2idx, idx2super, 2)
+a1_right = s_destroy_right(super2idx, idx2super, 1)
+a2_right = s_destroy_right(superidx, idx2super, 2)
+"""
+step4: define operators in the full hilbert space and compare. 
+To compare, I create operators in the usual way in the full hilbert space and I apply them to states that belong to my reduced super space 
+and check if the two results match. So I have to understand how to define the states in the vector representation that belong to the reduced super space
+"""
